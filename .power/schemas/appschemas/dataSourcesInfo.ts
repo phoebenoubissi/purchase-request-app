43640f824f52a8891922640282c7f5b29b793429
phoebenoubissi <phoebesi20@gmail.com>
2026-04-23 16:28:05 -0400

    Updates

---
+++ .power/schemas/appschemas/dataSourcesInfo.ts
@@ -0,0 +1,50 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * This file is auto-generated. Do not modify it manually.
+ * Changes to this file may be overwritten.
+ */
+
+export const dataSourcesInfo = {
+  "cr38e_masterorderingcatalogs": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr38e_masterorderingcatalogid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cr38e_projects": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr38e_projectid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cr38e_purchaserequestapprovals": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr38e_purchaserequestapprovalid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cr38e_purchaserequestdetails": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr38e_purchaserequestdetailid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cr38e_purchaserequests": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr38e_purchaserequestid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cr38e_richtextfiles": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr38e_richtextfileid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  }
+};
